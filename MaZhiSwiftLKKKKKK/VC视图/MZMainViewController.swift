a5145e973472717b24a264e5da3eea87b2608665
mazhi <humingx@yeah.net>
2018-02-28 17:48:27 +0800

    修改长度

--- MaZhiSwiftLKKKKKK/VC视图/MZMainViewController.swift
+++ MaZhiSwiftLKKKKKK/VC视图/MZMainViewController.swift
@@ -17,7 +17,7 @@ class MZMainViewController: UIViewController,ChargeBgColorDelegate {
         self.view.backgroundColor = UIColor.white
         
         let textButton:UIButton = UIButton()
-        textButton.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+        textButton.frame = CGRect(x: 110, y: 100, width: 200, height: 200)
         textButton.backgroundColor = UIColor.blue
         textButton.addTarget(self, action: #selector(actionAnimaion), for: UIControlEvents.touchUpInside)
         self.view .addSubview(textButton)
